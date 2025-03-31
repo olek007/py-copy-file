f484de8f74fa09641871465dc3333e7186d6755f
Aleksander Rokita <kelor2@o2.pl>
2025-03-31 12:42:53 +0200

    add check if files are the same

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ def copy_file(command: str) -> None:
     try:
         terminal_command, file_to_copy, new_file = command.split(" ")
         parse_command_cp(terminal_command)
+        check_if_files_the_same(file_to_copy, new_file)
         with (open(file_to_copy, "r") as file_in,
               open(new_file, "w") as file_out):
             file_out.write(file_in.read())
@@ -14,3 +15,8 @@ def copy_file(command: str) -> None:
 def parse_command_cp(terminal_command: str) -> None:
     if terminal_command != "cp":
         raise ValueError(f"command '{terminal_command}' not equal 'cp'")
+
+
+def check_if_files_the_same(file1: str, file2: str) -> None:
+    if file1 == file2:
+        raise ValueError(f"the same file names: '{file1}', '{file2}'")

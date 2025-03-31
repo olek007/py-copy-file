83ce99c8ba3555fbcb58dd5971573dd570085d15
Aleksander Rokita <kelor2@o2.pl>
2025-03-31 12:36:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        terminal_command, file_to_copy, new_file = command.split(" ")
+        parse_command_cp(terminal_command)
+        with (open(file_to_copy, "r") as file_in,
+              open(new_file, "w") as file_out):
+            file_out.write(file_in.read())
+    except ValueError as e:
+        print(e)
+    except FileNotFoundError as e:
+        print(e)
+
+
+def parse_command_cp(terminal_command: str) -> None:
+    if terminal_command != "cp":
+        raise ValueError(f"command '{terminal_command}' not equal 'cp'")
